174e29add68791a1bd2341dcf083c9b8651f1d17
Matthias Rheinhardt <mreinhar@taito-gpu.csc.fi>
2019-06-26 15:11:32 +0300

    MR: added PENCIL_ASTAROTH

--- src/standalone/config_loader.cc
+++ src/standalone/config_loader.cc
@@ -153,6 +153,7 @@ update_config(AcMeshInfo* config)
     config->real_params[AC_sq2GM_star] = AcReal(sqrt(AcReal(2) * config->real_params[AC_GM_star]));
 
     const bool print_config = true;
+#ifndef PENCIL_ASTAROTH
     if (print_config) {
         printf("###############################################################"
                "\n");
@@ -161,6 +162,7 @@ update_config(AcMeshInfo* config)
         printf("###############################################################"
                "\n");
     }
+#endif
 }
 
 /**
