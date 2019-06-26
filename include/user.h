daddde15b8ba8cb4b9343bb35a6a8e49dc7006c9
Matthias Rheinhardt <mreinhar@taito-gpu.csc.fi>
2019-06-26 17:06:46 +0300

    MR: encapsulated user stuff

---
+++ include/user.h
@@ -0,0 +1,16 @@
+#ifdef PENCIL_ASTAROTH
+
+  #include "../cparam.inc_c.h"
+  #define NGHOST nghost
+  #define STENCIL_ORDER (2*nghost)
+  #include "PC_moduleflags.h"
+  #define CONFIG_PATH
+  #define AC_MULTIGPU_ENABLED (false)
+  #ifdef DOUBLE_PRECISION 
+    #define AC_DOUBLE_PRECISION 1
+  #else
+    #define AC_DOUBLE_PRECISION 0
+  #endif
+  #define USER_PROVIDED
+#endif
+
