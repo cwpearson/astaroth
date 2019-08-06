13c1bf272b291ca6482d0b30170c04567857f65f
jpekkila <johannes.pekkila@aalto.fi>
2019-08-06 17:44:43 +0300

    Removed old/outdated/unused scripts

--- scripts/fix_style.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $1 == "DO" && $2 == "IT!" ]]; then
-    find -name \*.h -o -name \*.cc -o -name \*.cu -o -name \*.cuh | xargs clang-format-6.0 -i -style=file
-    echo "It is done."
-else
-    find -name \*.h -o -name \*.cc -o -name \*.cu -o -name \*.cuh
-    echo "I'm going to try to fix the style of these files."
-    echo "If you're absolutely sure, give \"DO IT!\" (without quotes) as a parameter."
-fi
