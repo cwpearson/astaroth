eefd60983a6e0ebaf9abda1277271b2eaa4c1a60
jpekkila <johannes.pekkila@aalto.fi>
2019-09-18 09:06:03 +0300

    3rd party dependencies do not have to be fetched manually any more, cmake make will do that automatically if needed

--- 3rdparty/setup_dependencies.sh
+++ 3rdparty/setup_dependencies.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-INITIAL_DIR=$(pwd)
-
-
-# Fetch SDL2
 git clone https://github.com/davidsiaw/SDL2.git
 cd SDL2
 git pull
-mkdir build
-cd build && cmake .. && make -j
-
-# See https://github.com/davidsiaw/SDL2/blob/master/docs/README-linux.md
-# if there are isses with building
-
-
-# Done
-cd $INITIAL_DIR
