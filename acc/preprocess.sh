b667735906503ee3efe807b205de2f74507dcc83
jpekkila <johannes.pekkila@aalto.fi>
2019-10-08 00:31:15 +0300

    Removed debug prints from the preprocessing script

--- acc/preprocess.sh
+++ acc/preprocess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Preprocesses the give file using GCC. This script is usually automatically called in
 # ./compile.sh, but may be called also individually for debugging purposes.
-cat $1 | gcc ${@:2} -x c -E - | sed "s/#.*//g" | tee ${PWD}/$(basename -- "$1").preprocessed
+cat $1 | gcc ${@:2} -x c -E - | sed "s/#.*//g" > $(basename -- "$1").preprocessed  #| tee ${PWD}/$(basename -- "$1").preprocessed
