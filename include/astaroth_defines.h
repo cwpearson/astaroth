6ed3b7978dde0eef0d703b2bd37455248009da94
jpekkila <johannes.pekkila@aalto.fi>
2019-10-07 15:44:21 +0300

    Updated the name of the generated header

--- include/astaroth_defines.h
+++ include/astaroth_defines.h
@@ -68,7 +68,7 @@ typedef struct {
     AcReal3 row[3];
 } AcMatrix;
 
-#include "stencil_defines.h" // User-defined header
+#include "user_defines.h" // User-defined header
 
 // clang-format off
 #define AC_FOR_BUILTIN_INT_PARAM_TYPES(FUNC)\
