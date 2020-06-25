c44c3d02b4d7879312279f03f52e2cf8133fe9fe
jpekkila <johannes.pekkila@protonmail.com>
2020-06-25 06:35:13 +0300

    Added a sample for testing the Fortran interface

---
+++ samples/fortrantest/main.f90
@@ -0,0 +1,23 @@
+program pc
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  include "astaroth.f90"
+
+  type(AcMeshInfo) :: info
+  type(c_ptr) :: device
+
+  print *, "Num int params"
+  print *, AC_NUM_INT_PARAMS
+
+  ! Setup config
+  info%int_params(AC_nx + 1) = 128
+  info%int_params(AC_ny + 1) = 128
+  info%int_params(AC_nz + 1) = 128
+  call acupdatebuiltinparams(info)
+
+  call acdevicecreate(0, info, device)
+  call acdeviceprintinfo(device)
+  call acdevicedestroy(device)
+
+end program
