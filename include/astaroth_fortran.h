1b50374cdbcea0008d5d720f0cdc37e24d5dbdad
jpekkila <johannes.pekkila@protonmail.com>
2020-06-25 20:09:35 +0300

    Added the rest of the basic functions required for running simulations with the fortran interface

--- include/astaroth_fortran.h
+++ include/astaroth_fortran.h
@@ -19,6 +19,30 @@ void acdevicedestroy_(Device* device);
 
 void acdeviceprintinfo_(const Device* device);
 
+void acupdatebuiltinparams_(AcMeshInfo* info);
+
+void acdeviceswapbuffers_(const Device* device);
+
+void acdeviceloadmesh_(const Device* device, const Stream* stream, const AcMesh* host_mesh);
+
+void acdevicestoremesh_(const Device* device, const Stream* stream, AcMesh* host_mesh);
+
+void acdeviceintegratesubstep_(const Device* device, const Stream* stream, const int* step_number,
+                               const int3* start, const int3* end, const AcReal* dt);
+void acdeviceperiodicboundconds_(const Device* device, const Stream* stream, const int3* start,
+                                 const int3* end);
+
+void acdevicereducescal_(const Device* device, const Stream* stream, const ReductionType* rtype,
+                         const VertexBufferHandle* vtxbuf_handle, AcReal* result);
+
+void acdevicereducevec_(const Device* device, const Stream* stream, const ReductionType* rtype,
+                        const VertexBufferHandle* vtxbuf0, const VertexBufferHandle* vtxbuf1,
+                        const VertexBufferHandle* vtxbuf2, AcReal* result);
+
+void acdevicesynchronizestream_(const Device* device, const Stream* stream);
+
+void acdeviceloadmeshinfo_(const Device* device, const AcMeshInfo* info);
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
